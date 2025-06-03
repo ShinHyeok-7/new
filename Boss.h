2e9bac2eb2bded1cb4548dbd2644148e76a19f38
ShinHyeok-7 <shbang0821@naver.com>
2025-06-03 19:57:38 +0900

    Boss.h

--- Boss.h
+++ Boss.h
@@ -5,7 +5,7 @@
 
 typedef struct {
     int x, y; // 보스의 위치
-    int width, height; // 보스의 크기(너비,, 높이이)
+    int width, height; // 보스의 크기(너비, 높이)
     int direction; // 이동방향(1:오른쪽, -1:왼쪽)
     int fire_cooldown; // 발사 대기 시간
     int hp; // 보스 현재 체력
@@ -23,7 +23,7 @@ extern Boss boss;
 extern int boss_active; // 보스가 현재 활성화 상태인지 아닌지 나타냄
 extern int round; // 몇번째 라운드인지 나타냄
 extern int enemies_remaining; // 남아있는 적의 수를 나타냄
-extern Bullet bullets[MAX_BULLETS]; // 보스가 발사하는 총알 정보 구조체체
+extern Bullet bullets[MAX_BULLETS]; // 보스가 발사하는 총알 정보 구조체
 
 // 보스 관련 함수들 //
 void init_boss();
